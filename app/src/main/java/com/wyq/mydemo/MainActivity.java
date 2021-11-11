bc7ec9c897bfa74e0702e3f31e1726920f87f298
王永琪 <yqwangaw@isoftstone.com>
2021-11-11 16:06:36 +0800

    手写注解绑定框架

--- app/src/main/java/com/wyq/mydemo/MainActivity.java
+++ app/src/main/java/com/wyq/mydemo/MainActivity.java
@@ -4,24 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.wyq.annotation_lib.Inject;
+import com.wyq.annotation_lib.annatation_common.OnClickCommon;
+import com.wyq.annotation_lib.annatation_common.OnLongClickCommon;
+import com.wyq.annotation_lib.annotation.BindView;
+import com.wyq.annotation_lib.annotation.Click;
+import com.wyq.annotation_lib.annotation.ContentView;
 import com.wyq.mydemo.http.HttpCallback;
 import com.wyq.mydemo.http.HttpHelper;
 import com.wyq.mydemo.http.ResponseData;
 
 import java.util.HashMap;
 
+@ContentView(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bt_test1)
+    Button button0;
+
+    @BindView(R.id.bt_test3)
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Inject.inject(this);
     }
 
     //发起网络请求
-    public void onclick(View view) {
+    @Click(R.id.bt_test1)
+    private void show() {
         String url = "https://v.juhe.cn/historyWeather/citys";
         HashMap<String, Object> params = new HashMap<>();
         params.put("province_id", "2");
@@ -33,6 +48,27 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, objResult.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-
     }
+
+    @Click(R.id.bt_test3)
+    private void show3() {
+        Toast.makeText(this, "show3 is run", Toast.LENGTH_SHORT).show();
+    }
+
+    @Click(R.id.bt_test4)
+    private void show4() {
+        Toast.makeText(this, "show4 is run", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClickCommon(R.id.bt_t1) // 点击事件
+    private void test1() {
+        Toast.makeText(this, "兼容事件 点击", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnLongClickCommon(R.id.bt_t2) // 长按事件
+    private boolean test2() {
+        Toast.makeText(this, "兼容事件 长按", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
 }
